4b65385fbc59a58f5831a59f96c6718d3cb2d5eb
pratikdhale07 <pratikdhale1651@gmail.com>
2025-03-03 18:46:43 +0530

    second commit

--- src/main/java/org/darker/entity/Expense.java
+++ src/main/java/org/darker/entity/Expense.java
@@ -1,5 +1,28 @@
 package org.darker.entity;
 
-public class Expense {
+import org.darker.enums.ExpenseCategory;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="expenses")
+public class Expense {
+   
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name="user_id", nullable=false)
+	private User user;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable=false)
+	private ExpenseCategory category;
+	
+	@Column(nullable=false)
+	private double amount;
+	
+	
+   
 }
